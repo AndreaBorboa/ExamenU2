0e2765b7e769bbec082244ba94ea58e0cce39b68
Andrea Borboa Ramírez <andrea.borboar@potros.itson.edu.mx>
2022-03-21 22:52:53 -0700

    Intento de vistaproducto

--- app/src/main/java/borboa/andrea/examenu2/detalle_platillos.kt
+++ app/src/main/java/borboa/andrea/examenu2/detalle_platillos.kt
@@ -2,12 +2,18 @@ package borboa.andrea.examenu2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Window
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 
 class detalle_platillos : AppCompatActivity() {
+
+    var precio : Double = 0.0
+    var total : Double = 0.0
+    var cantidad : Int = 1
+
     val iv_imagen_item: ImageView = findViewById(R.id.imagen_selected)
     val tv_nombre_item: TextView = findViewById(R.id.item_selected)
     val tv_precio_item: TextView = findViewById(R.id.item_precio)
@@ -20,17 +26,24 @@ class detalle_platillos : AppCompatActivity() {
         setContentView(R.layout.item_seleccion)
 
         val bundle=intent.extras
+
         if(bundle!=null){
-            iv_imagen_item.setImageResource(bundle.getInt("image_item"))
-            tv_nombre_item.setText(bundle.getString("nombre_item"))
-            tv_precio_item.setText(bundle.getString("precio_item"))
+            var comida = bundle.getString("nombre")
+            if(comida != null){
+                Log.d("Nombre del platillo",comida)
+            }
+
+            iv_imagen_item.setImageResource(bundle.getInt("imagen"))
+            tv_nombre_item.setText(bundle.getString("nombre").toString())
+            et_cantidad.setText(bundle.getString("cantidad").toString())
+
+            precio = bundle.getDouble("precio")
+            total = bundle.getDouble("total")
+
+            tv_precio_item.setText(precio.toString())
+            tv_total.setText(total.toString())
+            var total1=cantidad*precio
+
         }
-
-        var cantidad = Integer.parseInt(et_cantidad.text.toString())
-        var precio = Integer.parseInt(tv_precio_item.text.toString())
-
-        var total=cantidad*precio
-        tv_total.setText(total.toString())
-
     }
 }
